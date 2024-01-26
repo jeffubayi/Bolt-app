b6688133f94997d8112c8b5d247c9521062c51a3
jeffubayi <jeffubayi@gmail.com>
2024-01-26 12:19:16 +0300

    add component assests

---
+++ constants/Colors.ts
@@ -0,0 +1,19 @@
+const tintColorLight = '#32D286';
+const tintColorDark = '#32D286';
+
+export default {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+    tabIconDefault: '#32D286',
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+    tint: tintColorDark,
+    tabIconDefault: '#32D286',
+    tabIconSelected: tintColorDark,
+  },
+};
